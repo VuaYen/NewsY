76cbf1a11b4ed7e060c47445580ed9954d8caa60
JohnPham <johnpham@JohnPhams-MacBook-Pro.local>
2021-03-12 10:39:24 -0800

    04_06e

--- builds/codeclinic/js/script.js
+++ builds/codeclinic/js/script.js
@@ -1,135 +1,108 @@
 (function() {
-  let fromDate = '2015-01-01';
-  let fromTime = '01:00:00';
+  var mySoundContext,
+    myOscillator,
+    myGain,
+    myDistortion,
+    myOscillator,
+    originalYPos,
+    originalXPos,
+    originalFrequency,
+    scaleFrequencies = [
+      110,
+      123.47,
+      130.81,
+      146.83,
+      164.81,
+      174.61,
+      196,
+      220,
+      246.94,
+      261.63,
+      293.66,
+      329.63,
+      349.23,
+      392,
+      440,
+      493.88,
+      523.25,
+      587.33,
+      659.25,
+      698.46,
+      783.99,
+      880,
+      987.77,
+      1046.5,
+      1174.66,
+      1318.51,
+      1396.91,
+      1567.98,
+      1760
+    ];
 
-  let toDate = '2015-01-01';
-  let toTime = '10:00:00';
+  var contextClass =
+    window.AudioContext || window.webkitAudioContext;
 
-  document.querySelector(
-    '#fromDate'
-  ).value = moment(fromDate, 'YYYY-MM-DD').format(
-    'YYYY-MM-DD'
-  );
+  var appNode = document.querySelector('.app');
+  var appWidth = appNode.offsetWidth;
 
-  document.querySelector(
-    '#fromTime'
-  ).value = moment(fromTime, 'hh:mm:ss').format(
-    'hh:mm:ss'
-  );
+  appNode.style.background =
+    'repeating-linear-gradient(to right, #FDF6E4, #FDF6E4 50%, #F7EFD7 50%, #F7EFD7)';
+  appNode.style.backgroundSize =
+    appWidth / scaleFrequencies.length * 2 + 'px 100%';
 
-  document.querySelector(
-    '#toDate'
-  ).value = moment(toDate, 'YYYY-MM-DD').format(
-    'YYYY-MM-DD'
-  );
-
-  document.querySelector(
-    '#toTime'
-  ).value = moment(toTime, 'hh:mm:ss').format(
-    'hh:mm:ss'
-  );
-
-  function generateChart(data) {
-    c3.generate({
-      data: {
-        y: 'barometric_pressure',
-        x: 'dates',
-        xFormat: '%Y-%m-%d %H:%M:%S',
-        json: data,
-        type: 'scatter',
-        types: {
-          barometric_pressure: 'scatter',
-          coefficient: 'line'
-        }
-      },
-      point: {
-        show: false
-      },
-      axis: {
-        x: {
-          type: 'timeseries',
-          tick: {
-            count: 4,
-            format: '%Y-%m-%d %H:%M:%S'
-          }
-        }
-      },
-      subchart: {
-        show: true //subchart
-      }
-    });
+  if (contextClass) {
+    mySoundContext = new contextClass();
+  } else {
+    document.querySelector('.app').innerHTML =
+      '<div class="container alert alert-danger">Sorry, this app requires the Web Audio API, which your browser does not support</div>';
   }
 
-  function loadChart() {
-    fetch('http://pixelprowess.com/i/lake.php', {
-      method: 'POST',
-      body: JSON.stringify({
-        fromDate: fromDate + ' ' + fromTime,
-        toDate: toDate + ' ' + toTime
-      })
-    })
-      .then(response => response.json())
-      .then(response => {
-        let pressure =
-          response.barometric_pressure;
-        let sum = pressure.reduce(
-          (a, b) => a + b
-        );
-        let size = pressure.length;
-        let avg = sum / size;
-        let begx = (pressure[0] + avg) / 2;
-        let endx = (pressure[size - 1] + avg) / 2;
-        let slope = (endx - begx) / size;
+  appNode.addEventListener('mousedown', e => {
+    if (myOscillator) {
+      myOscillator.stop();
+    }
 
-        // response.average = pressure.map(
-        //   () => avg
-        // );
+    mouseXPos = e.clientX;
+    mouseYPos = e.clientY;
+    originalYPos = mouseYPos;
 
-        // response.delta = pressure.map(
-        //   x => (x + avg) / 2
-        // );
+    originalFrequency =
+      scaleFrequencies[
+        Math.floor(
+          mouseXPos / appWidth * scaleFrequencies.length
+        )
+      ];
 
-        response.coefficient = pressure.map(
-          (x, i) => begx + i * slope
-        );
-        generateChart(response);
-      })
-      .catch(error => console.error(error));
-  }
+    myOscillator = mySoundContext.createOscillator();
+    myOscillator.type = 'sine';
+    myOscillator.frequency.value = originalFrequency;
+    myOscillator.start();
 
-  document
-    .querySelector('#fromDate')
-    .addEventListener('blur', () => {
-      fromDate = document.querySelector(
-        '#fromDate'
-      ).value;
-      loadChart();
+    myGain = mySoundContext.createGain();
+    myGain.gain.value = 0.1;
+
+    myDistortion = mySoundContext.createWaveShaper();
+
+    myOscillator.connect(myDistortion);
+    myDistortion.connect(myGain);
+    myGain.connect(mySoundContext.destination);
+
+    appNode.addEventListener('mousemove', e => {
+      var distanceY = e.clientY - originalYPos;
+      mouseXPos = e.clientX;
+      appWidth = appNode.offsetWidth;
+
+      myGain.gain.value = mouseXPos / appWidth;
+      myOscillator.frequency.value =
+        originalFrequency + distanceY;
+
+      appNode.style.backgroundSize =
+        appWidth / scaleFrequencies.length * 2 + 'px 100%';
     });
+  });
 
-  document
-    .querySelector('#fromTime')
-    .addEventListener('blur', () => {
-      fromTime = document.querySelector(
-        '#fromTime'
-      ).value;
-      loadChart();
-    });
-
-  document
-    .querySelector('#toDate')
-    .addEventListener('blur', () => {
-      toDate = document.querySelector('#toDate')
-        .value;
-      loadChart();
-    });
-
-  document
-    .querySelector('#toTime')
-    .addEventListener('blur', () => {
-      toTime = document.querySelector('#toTime')
-        .value;
-      loadChart();
-    });
-
-  loadChart();
+  appNode.addEventListener('mouseup', e => {
+    myOscillator.stop();
+    appNode.removeEventListener('mousemove', null);
+  });
 })();
